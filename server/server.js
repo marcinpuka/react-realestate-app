2095eb9a50f05e9bce5c14f2717e7676c34db955
Marcin Puka <marcinpuka@gmail.com>
2017-11-07 14:20:46 +0100

    Add heroku script

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
+
+
 
 app.use(express.static(publicPath));
 
@@ -9,7 +12,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(3005, () => {
-    console.log(publicPath);
+app.listen(port, () => {
     console.log('server is up!');
 });
