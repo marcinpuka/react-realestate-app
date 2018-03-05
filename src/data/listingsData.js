1d3a4c5e590a0dfed63cd28c8933ef0512e1621f
Marcin Puka <marcinpuka@gmail.com>
2018-03-05 10:14:37 +0100

    pic opt

--- src/data/listingsData.js
+++ src/data/listingsData.js
@@ -13,7 +13,7 @@ var listingsData = [
             'wardrobe'
         ], 
         homeType: 'Apartment', 
-        image: "https://cdn.homedit.com/wp-content/uploads/2016/10/Penthouse-apartment-in-church-staircase-and-mezzanine.jpg"
+        image: 'src="../../public/images/bootstrap.jpg"'
 
     }, 
     {
